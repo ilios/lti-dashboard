2a0f39fb11ca78909759a0c6ccc50976da4fbe44
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-05-21 17:17:52 -0700

    rm obsolete controller. re-export routes/controlles from common.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,1 @@
-import Route from '@ember/routing/route';
-import DashboardRoute from 'ilios-common/mixins/dashboard-route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
-export default Route.extend(DashboardRoute, AuthenticatedRouteMixin);
+export { default } from 'ilios-common/routes/dashboard';
